8ff8a7bfe7fdbcde53ee326a391fc3338d6c1974
uxsasukexnaruto <nguyha16@gmail.com>
2020-06-05 10:49:30 -0700

    change ip and endpoints

--- server/index.js
+++ server/index.js
@@ -11,63 +11,53 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-const room = 'http://54.191.38.196',
-    booking = 'http://54.191.38.196',
+const room = 'http://54.189.142.212',
+    booking = 'http://54.189.142.212',
     listings = 'http://18.237.95.59',
     photos = 'http://34.214.44.203';
 
-app.get('/api/', (req, res) => {
-    const { id } = req.query;
-    const promise1 = axios.get(room.concat(`/?id=${id}`));
-    const promise2 = axios.get(booking.concat(`/?id=${id}`));
-    const promise3 = axios.get(listings.concat(`/?id=${id}`));
-    const promise4 = axios.get(photos.concat(`/?id=${id}`));
-    Promise.all([promise1, promise2, promise3, promise4])
-    .then((values) => {
-        console.log(values);
-        res.send(values);
+// app.get('/api/', (req, res) => {
+//     const { id } = req.query;
+//     const promise1 = axios.get(room.concat(`/?id=${id}`));
+//     const promise2 = axios.get(booking.concat(`/?id=${id}`));
+//     const promise3 = axios.get(listings.concat(`/?id=${id}`));
+//     const promise4 = axios.get(photos.concat(`/?id=${id}`));
+//     Promise.all([promise1, promise2, promise3, promise4])
+//     .then((values) => {
+//         console.log(values);
+//         res.send(values);
+//     })
+//     .catch((err) => {
+//         res.status(500).send(err);
+//     });
+// });
+
+
+
+
+
+app.get('/:id', (req, res) => {
+    const { id } = req.params;
+    // console.log(id);
+    let room = `http://54.189.142.212/booking/?id=${id}`,
+        booking = `http://54.189.142.212/room/?id=${id}`,
+        listings = `http://54.186.187.224/listings/${id}`,
+        photos = `http://54.149.5.250/photos/?id=${id}`;
+
+    const promise1 = axios.get(room);
+    const promise2 = axios.get(booking);
+    const promise3 = axios.get(listings);
+
+    Promise.all([promise1, promise2, promise3])
+    .then((result) => {
+        console.log(result);
+        res.send(result[0].data);
     })
     .catch((err) => {
         res.status(500).send(err);
-    });
+    })
 });
 
-
-
-
-
-// app.get('/:id', (req, res) => {
-//     const { id } = req.params;
-//     // console.log(id);
-//     const listings = `http://34.214.44.203/photos/${id}`;
-//     const description = axios.get(listings);
-    
-//     Promise.all([description])
-//     .then((result) => {
-//         console.log(result.status);
-//         res.send(result.data);
-//     })
-    
-// });
-// app.get('/:id', (req, res) => {
-//     const room = 'http://54.191.38.196',
-//     //   booking = 'http://54.191.38.196',
-//       listings = `http://35.162.174.32/listings/${id}`;
-//     //   photos = 'http://34.214.44.203';
-
-//     // const photos = axios.get(photos);
-//     // const booking = axios.get(booking);
-//     const description = axios.get(listings);
-
-//     Promise.all([description])
-//     .then((result) => {
-//         console.log(result);
-//         res.send(result);
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//     });
-// });
 
 
 app.listen(PORT, () => {
