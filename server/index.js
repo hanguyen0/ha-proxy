92f40759a1950e0a762dac83f2fb9d8ae97b9d12
uxsasukexnaruto <nguyha16@gmail.com>
2020-06-05 10:50:42 -0700

    delete comments

--- server/index.js
+++ server/index.js
@@ -11,30 +11,6 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-const room = 'http://54.189.142.212',
-    booking = 'http://54.189.142.212',
-    listings = 'http://18.237.95.59',
-    photos = 'http://34.214.44.203';
-
-// app.get('/api/', (req, res) => {
-//     const { id } = req.query;
-//     const promise1 = axios.get(room.concat(`/?id=${id}`));
-//     const promise2 = axios.get(booking.concat(`/?id=${id}`));
-//     const promise3 = axios.get(listings.concat(`/?id=${id}`));
-//     const promise4 = axios.get(photos.concat(`/?id=${id}`));
-//     Promise.all([promise1, promise2, promise3, promise4])
-//     .then((values) => {
-//         console.log(values);
-//         res.send(values);
-//     })
-//     .catch((err) => {
-//         res.status(500).send(err);
-//     });
-// });
-
-
-
-
 
 app.get('/:id', (req, res) => {
     const { id } = req.params;
