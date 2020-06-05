f45e21af40b24ba87b3cea2690da8f87d517e7f6
uxsasukexnaruto <nguyha16@gmail.com>
2020-06-04 21:13:23 -0700

    refactor server

--- server/index.js
+++ server/index.js
@@ -1,49 +1,75 @@
 require('newrelic');
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+
 const app = express();
-var cors = require('cors')
-app.use(cors())
+const PORT = process.env.PORT || 8000;
+app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/../public'));
+app.use(express.static(path.join(__dirname, '../public')));
 
-// app.get('/:id', function(req, res) {
-//     res.sendFile(path.join(__dirname, '/../public/index.html'));
-// });
+const room = 'http://54.191.38.196',
+    booking = 'http://54.191.38.196',
+    listings = 'http://18.237.95.59',
+    photos = 'http://34.214.44.203';
 
-
-// app.get('/photos/:listingID', (req, res) => {
-//     const { listingID } = req.params;
-//     res.redirect(`http://localhost:3001/photos/${listingID}`);
-// });
-
-app.get('/listings/:id', (req, res) => {
-    const { id} = req.params;
-    //http://54.188.63.61:3002/
-    res.redirect(`http://54.188.63.61:3002/listings/${id}`);
+app.get('/api/', (req, res) => {
+    const { id } = req.query;
+    const promise1 = axios.get(room.concat(`/?id=${id}`));
+    const promise2 = axios.get(booking.concat(`/?id=${id}`));
+    const promise3 = axios.get(listings.concat(`/?id=${id}`));
+    const promise4 = axios.get(photos.concat(`/?id=${id}`));
+    Promise.all([promise1, promise2, promise3, promise4])
+    .then((values) => {
+        console.log(values);
+        res.send(values);
+    })
+    .catch((err) => {
+        res.status(500).send(err);
+    });
 });
 
-// app.get('/room/:id', (req, res) => {
-//     const { id} = req.params;
-//     res.redirect(`http://localhost:3003/room/${id}`);
+
+
+
+
+// app.get('/:id', (req, res) => {
+//     const { id } = req.params;
+//     // console.log(id);
+//     const listings = `http://34.214.44.203/photos/${id}`;
+//     const description = axios.get(listings);
+    
+//     Promise.all([description])
+//     .then((result) => {
+//         console.log(result.status);
+//         res.send(result.data);
+//     })
+    
+// });
+// app.get('/:id', (req, res) => {
+//     const room = 'http://54.191.38.196',
+//     //   booking = 'http://54.191.38.196',
+//       listings = `http://35.162.174.32/listings/${id}`;
+//     //   photos = 'http://34.214.44.203';
+
+//     // const photos = axios.get(photos);
+//     // const booking = axios.get(booking);
+//     const description = axios.get(listings);
+
+//     Promise.all([description])
+//     .then((result) => {
+//         console.log(result);
+//         res.send(result);
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
 // });
 
-// app.get('/booking/:id', (req, res) => {
-//     const { id} = req.params;
-//     res.redirect(`http://localhost:3003/booking/${id}`);
-// });
 
-// app.get('/reviews/:room_id', (req, res) => {
-//     const { room_id} = req.params;
-//     res.redirect(`http://localhost:3004/reviews/${room_id}`);
-// });
-
-// app.get('/MoreHomes/:listingID', (req, res) => {
-//     const { listingID} = req.params;
-//     res.redirect(`http://localhost:3005/MoreHomes/${listingID}`);
-// });
-
-
-app.listen(3010, function() {
-    console.log('listening on port 3010!');
-  }); 
+app.listen(PORT, () => {
+  console.log('Proxy listening on port 8000');
+});
