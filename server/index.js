a4c6db258d3a6a08884e756a4edf984100fb3b70
uxsasukexnaruto <nguyha16@gmail.com>
2020-06-05 10:58:36 -0700

    add 4th id

--- server/index.js
+++ server/index.js
@@ -18,13 +18,14 @@ app.get('/:id', (req, res) => {
     let room = `http://54.189.142.212/booking/?id=${id}`,
         booking = `http://54.189.142.212/room/?id=${id}`,
         listings = `http://54.186.187.224/listings/${id}`,
-        photos = `http://54.149.5.250/photos/?id=${id}`;
+        photos = `http://54.149.5.250/photos/${id}`;
 
     const promise1 = axios.get(room);
     const promise2 = axios.get(booking);
     const promise3 = axios.get(listings);
+    const promise4 = axios.get(photos);
 
-    Promise.all([promise1, promise2, promise3])
+    Promise.all([promise1, promise2, promise3, promise4])
     .then((result) => {
         // console.log(result);
         res.send(result[0].data);
